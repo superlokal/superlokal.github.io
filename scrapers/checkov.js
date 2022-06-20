52289186afa048cb073f6f3ead7962ced59423f0
Krispin Schulz <krispin.schulz@avenga.com>
2022-06-20 13:04:57 +0200

    fetch data

--- scrapers/checkov.js
+++ scrapers/checkov.js
@@ -3,7 +3,13 @@ module.exports = () => {
   const result = [
     ['2022-04-24', '10:00', 'Frühjahrsputz Chekov'],
     ['2022-05-21', '22:00', 'KYMATIK goes CHEKOV'],
-    ['2022-06-03', '16:00', '... The unoptimal Festival !']
+    ['2022-06-03', '16:00', '... The unoptimal Festival !'],
+    ['2022-06-23', '18:00', 'Fyahamnd | Vortrag'],
+    ['2022-06-30', '18:00', 'Fyahamnd | Punk aus der Dose'],
+    ['2022-07-07', '18:00', 'Fyahamnd | Liedermacher'],
+    ['2022-07-14', '18:00', 'Fyahamnd | Nysmus und Hain’ze'],
+    ['2022-07-30', '20:00', 'Los Fastidios + The Twinkles + Die fickenden Turnschuhe'],
+    ['2022-09-29', '20:00', 'HECKSPOILER "Tokyo Drift" Cottbus'],
   ]
   return result.map((arr) => {
     const [datum, zeit, info] = arr
--- scrapers/fango.js
+++ scrapers/fango.js
@@ -1,10 +1,10 @@
 module.exports = () => {
   const lokation = 'Galerie Fango'
   const result = [
-    ['2022-04-08', '20:00', 'Concerto Fango: Mummys A Tree'],
     ['2022-04-14', '20:00', 'Jam Session'],
     ['2022-04-30', '20:00', 'Concerto Fango: Lost But Grounde'],
     ['2022-06-02', '20:00', 'Concerto Fango: Three For Silver'],
+    ['2022-09-09', '20:00', 'Concerto Fango: Mummys A Tree'],
     ['2022-09-30', '20:00', 'Concerto Fango: Jonas Ringtved']
   ]
   return result.map((arr) => {
