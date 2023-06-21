5525cb88866c4db8bb01f5ff0554d3c8c08a3104
Krispin Schulz <krispin@posteo.de>
2023-06-21 11:43:30 +0200

    Update fango.js

--- scrapers/fango.js
+++ scrapers/fango.js
@@ -1,11 +1,7 @@
 module.exports = () => {
   const lokation = 'Galerie Fango'
   const result = [
-    ['2022-04-14', '20:00', 'Jam Session'],
-    ['2022-04-30', '20:00', 'Concerto Fango: Lost But Grounde'],
-    ['2022-06-02', '20:00', 'Concerto Fango: Three For Silver'],
-    ['2022-09-09', '20:00', 'Concerto Fango: Mummys A Tree'],
-    ['2022-09-30', '20:00', 'Concerto Fango: Jonas Ringtved']
+    //['2022-04-14', '20:00', 'Jam Session'],
   ]
   return result.map((arr) => {
     const [datum, zeit, info] = arr
